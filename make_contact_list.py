6e37825c8529f1a8aee2b073d960c4dab18588bd
Tammer Kamel <tammer@tammer.com>
2026-02-04 05:52:44 -0500

    .

--- make_contact_list.py
+++ make_contact_list.py
@@ -9,10 +9,12 @@ OWNER_ID = "29286558" # Tammer
 
 date_limit = "2016-02-01T00:00:00.000Z"
 
+blacklist = ["Alex Wright","Daphne","Tammer"]
 
 def main():
     contacts = get_contacts_for_owner(OWNER_ID, date_limit)
     contacts = deduplicate_contacts(contacts)
+    contacts = [contact for contact in contacts if contact['name'] not in blacklist]
     print(json.dumps(contacts, indent=2))
     return contacts
 
