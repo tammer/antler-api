915ae426eae4376f3540fbeaf4c9369ff2eae004
Tammer Kamel <tammer@tammer.com>
2026-02-05 09:35:57 -0500

    .

---
+++ load_supa.py
@@ -0,0 +1,13 @@
+from meetgeek import get_all_meetings
+from supa_from_id import supa_from_id
+
+
+
+meetings = get_all_meetings()
+for meeting in meetings:
+    print(meeting["meeting_id"])
+    try:
+        y = supa_from_id(meeting["meeting_id"])
+        print(y['ids'], flush=True)
+    except Exception as e:
+        print(f"Error processing meeting {meeting['meeting_id']}: {e}", flush=True)
--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-from meetgeek import get_all_meetings
-from supa_from_id import supa_from_id
+from hubspot import get_contacts_for_owner
 
-
-
-meetings = get_all_meetings()
-for meeting in meetings:
-    print(meeting["meeting_id"])
-    try:
-        y = supa_from_id(meeting["meeting_id"])
-        print(y['ids'], flush=True)
-    except Exception as e:
-        print(f"Error processing meeting {meeting['meeting_id']}: {e}", flush=True)
+contacts = get_contacts_for_owner("29286558")
+print(contacts[0])
