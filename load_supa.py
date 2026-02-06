627eae4bb7146f8c32de3ac5fa9c2ce288d5ec6f
Tammer Kamel <tammer@tammer.com>
2026-02-06 07:04:31 -0500

    .

--- load_supa.py
+++ load_supa.py
@@ -1,11 +1,15 @@
 from meetgeek import get_all_meetings
 from supa_from_id import supa_from_id
+from supa import check_id
 
 
 
 meetings = get_all_meetings()
 for meeting in meetings:
     print(meeting["meeting_id"])
+    if check_id(meeting["meeting_id"]):
+        print("Already exists", flush=True)
+        continue
     try:
         y = supa_from_id(meeting["meeting_id"])
         print(y['ids'], flush=True)
