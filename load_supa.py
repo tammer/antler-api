55d28f0e072c93cbeb940abfb8f92f6a27b763c3
Tammer Kamel <tammer@tammer.com>
2026-02-11 14:53:34 -0500

    .

--- load_supa.py
+++ load_supa.py
@@ -2,10 +2,12 @@ from meetgeek import get_all_meetings
 from supa_from_id import supa_from_id
 from supa import check_id
 
-
+cut_off = "2026-02-10T00:00:00.000Z"
 
 meetings = get_all_meetings()
 for meeting in meetings:
+    if meeting["timestamp_start_utc"] < cut_off:
+        continue
     print(meeting["meeting_id"])
     if check_id(meeting["meeting_id"]):
         print("Already exists", flush=True)
