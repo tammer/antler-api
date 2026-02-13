aca0777ad75d5cddf45d7840cb41f00614732b27
Tammer Kamel <tammer@tammer.com>
2026-02-13 16:09:51 -0500

    .

--- load_supa.py
+++ load_supa.py
@@ -2,7 +2,7 @@ from meetgeek import get_all_meetings
 from supa_from_id import supa_from_id
 from supa import check_id
 
-cut_off = "2026-02-10T00:00:00.000Z"
+cut_off = "2026-02-11T00:00:00.000Z"
 
 meetings = get_all_meetings()
 for meeting in meetings:
